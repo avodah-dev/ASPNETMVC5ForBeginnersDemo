30711101e2cfac54ff78845e86825f32d70ca481
Nathan Bedford <nathan.bedford@gmail.com>
2025-03-08 09:07:08 -0500

    Upgraded to 4.8

--- MVC5RealWorld/Models/DB/DemoModel.Designer.cs
+++ MVC5RealWorld/Models/DB/DemoModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\ProudMonkey\Documents\Visual Studio 2015\Demo\MVC5RealWorld\MVC5RealWorld\Models\DB\DemoModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\dev\ASPNETMVC5ForBeginnersDemo\MVC5RealWorld\Models\DB\DemoModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
